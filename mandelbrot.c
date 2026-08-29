0cb9592888773db4c2fc8652227bebefcf123ebc
Daniel andrade <moongames12345@gmail.com>
2026-08-29 20:30:46 -0300

    1

---
+++ mandelbrot.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+    if(argc != 5){
+        fprintf(stderr, "Uso: %s [largura] [altura] [max_iteracoes] [num_threads]\n", argv[0]);
+        return 1;
+    }
+
+    int largura;
+    int altura;
+    int max_iteracoes;
+    int num_threads;
+
+    largura = atoi(argv[1]);
+    altura = atoi(argv[2]);
+    max_iteracoes = atoi(argv[3]);
+    num_threads = atoi(argv[4]);
+    if (largura <= 0 || altura <= 0 || max_iteracoes <= 0 || num_threads <= 0) {
+        fprintf(stderr, "Erro: parametros invalidos.\n");
+        return 1;
+    }
+     int *pixels;
+
+    pixels = malloc(largura * altura * sizeof(int));
+
+    if (pixels == NULL) {
+        fprintf(stderr, "Erro: falha na alocacao de memoria.\n");
+        return 1;
+    }
+
+    free(pixels);
+
+    return 0;
+    }
